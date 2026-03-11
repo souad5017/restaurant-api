588b02e8c7038b3fd6410409ec2cdcb5e41d60f9
unknown <souadelbarjiji@gmail.com>
2026-03-11 12:25:01 +0000

    BA-4 Register Endpoint

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    function register (Request $request) {
+        $request->validate([
+            'name'=> 'required|string|max:100',
+            'email'=> 'required|email|unique:users',
+            'password'=> 'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name'=>$request->name ,
+            'email'=>$request->email,
+            'password'=> Hash::make($request->password)
+        ]) ;
+
+        $token = $user->createToken('login-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token , 
+            'user'=> $user
+        ]);
+    }
+
+
 }
+
